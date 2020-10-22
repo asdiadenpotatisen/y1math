683eb759a9b326e762e852f32a7fb295455d78d8
asdiadenpotatisen <69951589+asdiadenpotatisen@users.noreply.github.com>
2020-10-22 13:55:38 +0800

    Update t07numberpatterns_powers2.py

--- t07numberpatterns_powers2.py
+++ t07numberpatterns_powers2.py
@@ -1,8 +1,9 @@
 # Generate powers of 2 from 1 to 10
 
 START = 1
-END = 10
+END =  10
 num = 2
 
 for i in range(START, END+1):
-    print(num ** i, end=' ')
+    if num**i <= END:
+        print(num**i, end='')
